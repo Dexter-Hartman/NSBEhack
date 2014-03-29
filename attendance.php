1d7099106d5463566e34de738a3271934baf1fee
Dex <Dex@172.21.151.10>
2014-03-29 07:59:15 -0500

    First Commit

--- attendance.php
+++ attendance.php
@@ -1,3 +1,15 @@
+<?php
+
+require('header.php');
+
+ $con=mysqli_connect("localhost","root","","mydb");
+ $db = mysql_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME) or die ('<p class="error">Error Connecting to the database'); 
+$dbc = mysqli_connect('localhost', 'root', '', 'mydb'); 
+mysql_select_db('mydb', $db) or die (mysql_error($db));
+?>
+
+
+
 <?PHP
 
 $default_status = 'checked'; //sets default radio button status to checked
@@ -9,4 +21,18 @@ $selected_radio = $_POST['name']; //when selected, the name of the student shoul
 //TODO: Determine how to deal with name given as well as date
 }
 
+?>
+<FORM name ="form1" method ="post" action ="radioButton.php">
+
+<Input type = 'Radio' Name ='name' value= 'Present'
+<?PHP print $default_status; ?>
+>Here
+
+<P>
+<Input type = "Submit" Name = "Submit1" VALUE = "Select a Radio Button">
+
+</FORM>
+
+<?php 
+require('footer.php');
 ?>
