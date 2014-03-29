722aa8d619a34e1c77f66eb4cf33a02353e81bfe
Dex <Dex@172.21.151.10>
2014-03-29 07:45:22 -0500

    First Commit

---
+++ attendance.php
@@ -0,0 +1,12 @@
+<?PHP
+
+$default_status = 'checked'; //sets default radio button status to checked
+
+if (isset($_POST['Submit1'])) {
+
+$selected_radio = $_POST['name']; //when selected, the name of the student should be pushed
+
+//TODO: Determine how to deal with name given as well as date
+}
+
+?>
